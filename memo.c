ba5d6abd42b59fc38a8f1481468455755d8445e6
alaa <alaamglal@gmail.com>
2023-12-27 16:26:48 -0800

    c

--- memo.c
+++ memo.c
@@ -5,9 +5,8 @@
  * Return: nothing
  */
 void memory_handler(int signum);
-void memory_handler(int signum)
+void memory_handler(int __attribute__((unused))signum)
 {
-	(void)signum;
 	signal(SIGINT, memory_handler);
 	write(STDIN_FILENO, "\n$ ", 3);
 }
